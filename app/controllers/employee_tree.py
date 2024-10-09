32e68a1ad28deca2a743cff3ef29cacd182de6f0
allen-strollby <allen.yesudasan@strollby.com>
2024-10-09 17:24:40 +0530

    feat: controller for people

--- app/controllers/employee_tree.py
+++ app/controllers/employee_tree.py
@@ -1,8 +1,14 @@
-from databases.enums import EntityTypeEnum
+from databases.documents.people import EntityModel
 
 
 def return_employee_tree(**kwargs):
+    user = kwargs.get("user")
     type = kwargs.get("type")
-    employee_type = EntityTypeEnum(type)
-    print(employee_type)
-
+    employee = EntityModel.objects(employee_id=user).get()
+    return {
+        "type": "PEOPLE",
+        "name": employee.name,
+        "status": employee.entity_status.value,
+        "room_id": employee.office.room_id,
+        "entity": type,
+    }
