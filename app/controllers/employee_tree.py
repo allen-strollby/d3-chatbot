5cdf97fdbe4eb4e0eb5b38fcd49077f4f28e9ec0
allen-strollby <allen.yesudasan@strollby.com>
2024-10-09 16:49:51 +0530

    feat: pulling user info context

--- app/controllers/employee_tree.py
+++ app/controllers/employee_tree.py
@@ -1,2 +1,8 @@
+from databases.enums import EntityTypeEnum
+
+
 def return_employee_tree(**kwargs):
-    print(kwargs.get("type"))
+    type = kwargs.get("type")
+    employee_type = EntityTypeEnum(type)
+    print(employee_type)
+
